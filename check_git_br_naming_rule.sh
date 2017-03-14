135cb04dc6645fd1c05dc7495f59826f42359827
narikin <yuki.iwnr@gmail.com>
2017-03-14 13:23:21 +0900

    implement

--- check_git_br_naming_rule.sh
+++ check_git_br_naming_rule.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 branches=`git br -r`
+remote="origin"
 
 for br in ${branches}
 do
-  echo "branch name: ${br}"
+  result="NG"
+
+  # feature
+  if [[ "${br}" =~ ^${remote}\/feature\/ ]];
+  then
+    result="OK"
+  fi
+
+  # release
+  if [[ "${br}" =~ ^${remote}\/release\/ ]];
+  then
+    result="OK"
+  fi
+
+  # result
+  echo "branch: ${br}"
+  echo "result: ${result}"
 done
